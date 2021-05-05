f7e78f592ca9b10be67fd195cb1de5c4efa75deb
hunghaubmt <voduchau01011997@gmail.com>
2021-05-05 12:41:21 +0700

    grid 16:9

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ import "../node_modules/react-grid-layout/css/styles.css"
 import "../node_modules/react-resizable/css/styles.css";
 
 import GridLayout from 'react-grid-layout';
-const maxHeight = 10;
+const maxHeight = 9;
+const maxWidth = 16;
 function App() {
   const [check, setCheck] = useState(0)
   const layout = [
-    { i: 'b', x: 0, y: 0, w: 3, h: 2, isBounded: true },
-    { i: 'c', x: 4, y: 0, w: 2, h: 2, isBounded: true },
-    { i: 'd', x: 4, y: 0, w: 2, h: 2 },
+    { i: 'b', x: 0, y: 0, w: 1, h: 1, isBounded: true },
+    { i: 'c', x: 1, y: 0, w: 1, h: 1, isBounded: true },
+    // { i: 'd', x: 2, y: 0, w: 1, h: 1 },
+    // { i: 'e', x: 3, y: 0, w: 1, h: 1 },
+    // { i: 'f', x: 4, y: 0, w: 1, h: 1 },
+    // { i: 'g', x: 5, y: 0, w: 1, h: 1 },
+    // { i: 'h', x: 6, y: 0, w: 1, h: 1 },
+    // { i: 'i', x: 7, y: 0, w: 1, h: 1 },
+    // { i: 'j', x: 8, y: 0, w: 1, h: 1 },
+    // { i: 'q', x: 9, y: 0, w: 1, h: 1 },
+    // { i: 'k', x: 10, y: 0, w: 1, h: 1 },
+    // { i: 'l', x: 11, y: 0, w: 1, h: 1 },
+    // { i: 'm', x: 12, y: 0, w: 1, h: 1 },
+    // { i: 'n', x: 13, y: 0, w: 1, h: 1 },
+    // { i: 'o', x: 14, y: 0, w: 1, h: 1 },
+    // { i: 'p', x: 15, y: 0, w: 1, h: 1 },
   ];
   const [lay, setLay] = useState(layout);
 
@@ -24,67 +38,53 @@ function App() {
     if (!(layout[layout.length - 1]
       && Object.keys(layout[layout.length - 1]).length === 0 && layout[layout.length - 1].constructor === Object)) {
       const temp = [...layout];
-      let checkHeight = [];
-      for (let i = 0; i < layout.length; i++) {
-        checkHeight.push(layout[i].h + layout[i].y);
-      }
-      for (let i = 0; i < temp.length; i++) {
-        if (temp[i].h + temp[i].y > maxHeight) {
-          while (temp[i].h + temp[i].y > maxHeight) {
-            if(check >= 6){
-              temp.splice(i,1);
-              setCheck(0);
-              break;
-            }
-            if (temp[i].h > 1) {
-              --temp[i].h;
+      let check = 0;
+      const CalHeight = (layParam) => {
+        console.log(layParam[1],'layParam')
+        for(let i = 0; i < layParam.length; i++) {
+          if(layParam[i].y + layParam[i].h > maxHeight){
+            if(layParam[i].h > 1){
+              --layParam[i].h;
             }
             else {
-              let time = 0;
-              while(temp[i].h + temp[i].y > maxHeight){
-                let check1 = check;
-                if(check >= 6){
-                  temp.splice(i,1);
-                  setCheck(0);
-                  break;
-                }
-                if(temp[i].x === 7){
-                  ++check1;
-                  setCheck(++check1);
-                  temp[i].x = 0;
-                }
-                else {
-                  temp[i].x++;
-                }
-                --temp[i].y;
-                temp[i].w = 1;
-                time++;
+              if(layParam[i].x < 15){
+                console.log(layParam[i].x,'xx')
+                ++layParam[i].x;
+                layParam[i].y = 0;
               }
-              break;
+              else {
+                layParam.splice(i,1);
+              }
             }
+            CalHeight(layParam)
+          }
+          else if(i === layParam.length - 1){
+            return layParam;
+          }
+          else {
+            continue;
           }
         }
       }
-      setLay(temp)
+      const rs = CalHeight(temp);
+      setLay(CalHeight(temp))
     }
 
   }
-  useEffect(() => {
-    console.log(lay,'lay')
-  },[lay])
+
   return (
     <div className="App">
       <div className="device-1">
         <GridLayout
           className="layout"
           layout={lay}
-          cols={8}
+          cols={16}
           isBounded={true}
           rowHeight={30}
           autoSize={false}
           width={500}
-          height={320}
-          margin={[2, 2]}
+          height={270}
+          margin={[0,0]}
           isDroppable={true}
           onLayoutChange={onLayoutChange}
           onDrop={(layout, item, e) => {
@@ -126,9 +126,9 @@ function App() {
         <button
           onClick={() => {
             const temp = [...lay];
-            let number = (lay.length) % 8;
+            let number = (lay.length) % (maxWidth);
 
-            console.log((lay.length) % 8, 'check');
+            console.log((lay.length) % (maxWidth), 'check');
             
             const item = {
               i: Math.random().toString(),
